84a0362f7d899363876add914d540dc2c6bf8387
Kevin Qi <kqi@amino.com>
2019-07-07 23:26:11 -0700

    have all document images sent to background script to label, try to overlay it (WIP)

--- public/background.js
+++ public/background.js
@@ -1,10 +1,24 @@
 mobilenet.load().then(model => {
   console.log("mobilenet model loaded");
+
+  // Content script message handling
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    const imageSrc = request.imageSrc;
+    convertImageSrcToImageData(imageSrc).then(imageData => {
+      model.classify(imageData).then(predictions => {
+        sendResponse({ predictions: predictions });
+      });
+    });
+    // Indicate that sendResponse is async
+    return true;
+  });
+
+  // Context menu handling
   chrome.contextMenus.create({
     title: "Label image with MobileNet",
     contexts: ["image"],
     onclick: function(item) {
-      convertBase64ImageToImageData(item.srcUrl).then(imageData => {
+      convertImageSrcToImageData(item.srcUrl).then(imageData => {
         model.classify(imageData).then(predictions => {
           console.log("predictions", predictions);
           alert(JSON.stringify(predictions, null, 4));
@@ -14,13 +28,13 @@ mobilenet.load().then(model => {
   });
 });
 
-function convertBase64ImageToImageData(dataURI) {
+function convertImageSrcToImageData(imageSrc) {
   return new Promise((resolve, reject) => {
     const canvas = document.createElement("canvas");
     const context = canvas.getContext("2d");
 
     const image = new Image();
-    image.src = dataURI;
+    image.src = imageSrc;
     image.onload = () => {
       canvas.width = image.width;
       canvas.height = image.height;
--- public/content.js
+++ public/content.js
@@ -1,1 +1,31 @@
 console.log("MobileNet content.js script loaded");
+
+for (let idx = 0; idx < document.images.length; idx++) {
+  const image = document.images[idx];
+
+  // Filter out small images
+  if (image.width > 50 && image.height > 50) {
+    chrome.runtime.sendMessage({ imageSrc: image.currentSrc }, response => {
+      console.log("response", response);
+
+      const imageParent = image.parentNode;
+      const container = document.createElement("div");
+      container.style.position = "relative";
+      container.style.width = `${image.width}px`;
+      container.style.height = `${image.height}px`;
+      const imageWrapper = document.createElement("div");
+      imageWrapper.style.position = "absolute";
+      const overlay = document.createElement("div");
+      overlay.style.width = "100%";
+      overlay.style.position = "absolute";
+      overlay.style.bottom = 0;
+      overlay.style.left = 0;
+      overlay.innerHTML = JSON.stringify(response);
+
+      imageWrapper.appendChild(image);
+      container.appendChild(imageWrapper);
+      container.appendChild(overlay);
+      imageParent.appendChild(container);
+    });
+  }
+}
