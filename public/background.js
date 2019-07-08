96e89f3a655dff3368c3634e596cab71b4e8d3f2
Kevin Qi <kqi@amino.com>
2019-07-07 20:56:54 -0700

    convert base64 image to ImageData to feed into mobilenet, allowing background script to run mobilenet instead of content script

--- public/background.js
+++ public/background.js
@@ -3,5 +3,31 @@ chrome.contextMenus.create({
   contexts: ["image"],
   onclick: function(item) {
     console.log("context", item);
+
+    Promise.all([
+      convertBase64ImageToImageData(item.srcUrl),
+      mobilenet.load()
+    ]).then(([imageData, model]) => {
+      console.log("mobilenet model loaded");
+      model.classify(imageData).then(predictions => {
+        console.log("predictions", predictions);
+      });
+    });
   }
 });
+
+function convertBase64ImageToImageData(dataURI) {
+  return new Promise((resolve, reject) => {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+
+    const image = new Image();
+    image.src = dataURI;
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      context.drawImage(image, 0, 0);
+      resolve(context.getImageData(0, 0, image.width, image.height));
+    };
+  });
+}
--- public/content.js
+++ public/content.js
@@ -1,11 +1,1 @@
-console.log("content.js loaded");
-
-document.addEventListener("contextmenu", event => {
-  console.log("contextmenu", event);
-
-  mobilenet.load().then(model => {
-    model.classify(event.target).then(predictions => {
-      console.log("predictions", predictions);
-    });
-  });
-});
+console.log("MobileNet content.js script loaded");
